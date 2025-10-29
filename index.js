8235ce1eae54cdcf8fe41affc9b764767d02bb95
Nancy Bankson <nbankson3@gmail.com>
2025-10-29 12:46:06 -0500

    Successfully added new object to table

--- index.js
+++ index.js
@@ -1,27 +1,40 @@
 // **********Declarations************************
-// let newTask = document.getElementById("new-task-el");
-// let newCategory = document.getElementById("new-category-el");
-// let newDate = document.getElementById("new-date-el");
-// let newStatus = document.getElementById("new-status-el");
-// let addButton = document.getElementById("button-el");
-let dummyObject = {
-    Task: "imatryintotype",
-    Category: "Work",
-    Date: "02/02/2225",
-    Status: "Complete",
-}
+let newTask = document.getElementById("new-task-el");
+let newCategory = document.getElementById("new-category-el");
+let newDate = document.getElementById("new-date-el");
+let newStatus = document.getElementById("new-status-el");
+let addButton = document.getElementById("button-el");
+let tableRow = document.getElementById("table-row-el")
+
+// let dummyObject = {
+//     Task: "imatryintotype",
+//     Category: "Work",
+//     Date: "02/02/2225",
+//     Status: "Complete",
+// }
 let taskDatabase = [];
-taskDatabase.push(dummyObject);
-console.log(taksDatabase);
+// taskDatabase.push(dummyObject);
+// console.log(taskDatabase);
 
 // **********Event listeners**********************
-addButton.addEventListener("click", function() {
+addButton.addEventListener("click", function () {
     let tableObject = {};
     tableObject.Task = newTask.value;
+    let displayTask = document.createElement("td");
+    displayTask.innerText = tableObject.Task;
+    tableRow.appendChild(displayTask);
     tableObject.Category = newCategory.value;
+    let displayCategory = document.createElement("td");
+    displayCategory.innerText = tableObject.Category;
+    tableRow.appendChild(displayCategory);
     tableObject.Date = newDate.value;
+    let displayDate = document.createElement("td");
+    displayDate.innerText = tableObject.Date;
+    tableRow.appendChild(displayDate);
     tableObject.Status = newStatus.value;
+    let displayStatus = document.createElement("td");
+    displayStatus.innerText = tableObject.Status;
+    tableRow.appendChild(displayStatus);
     taskDatabase.push(tableObject)
+    console.log(taskDatabase)
 })
-
-console.log(taksDatabase)
