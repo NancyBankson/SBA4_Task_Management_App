b65c9ea6032252b13d78aaa76ffcf95bfb1cb121
Nancy Bankson <nbankson3@gmail.com>
2025-10-29 11:05:48 -0500

    Working on event listener

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 // **********Declarations************************
-let newTask = document.getElementById("new-task-el");
-let newCategory = document.getElementById("new-category-el");
-let newDate = document.getElementById("new-date-el");
-let newStatus = document.getElementById("new-status-el");
-let addButton = document.getElementById("button-el");
-let taskDatabase = {};
+// let newTask = document.getElementById("new-task-el");
+// let newCategory = document.getElementById("new-category-el");
+// let newDate = document.getElementById("new-date-el");
+// let newStatus = document.getElementById("new-status-el");
+// let addButton = document.getElementById("button-el");
+let dummyObject = {
+    Task: "imatryintotype",
+    Category: "Work",
+    Date: "02/02/2225",
+    Status: "Complete",
+}
+let taskDatabase = [];
+taskDatabase.push(dummyObject);
+console.log(taksDatabase);
 
 // **********Event listeners**********************
 addButton.addEventListener("click", function() {
@@ -13,5 +21,7 @@ addButton.addEventListener("click", function() {
     tableObject.Category = newCategory.value;
     tableObject.Date = newDate.value;
     tableObject.Status = newStatus.value;
+    taskDatabase.push(tableObject)
 })
 
+console.log(taksDatabase)
